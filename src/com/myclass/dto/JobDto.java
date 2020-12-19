2da18fc88aaf9a5e400cf047bc585e9999936436
donglong-ops <59483840+donglong-ops@users.noreply.github.com>
2020-12-19 16:55:25 +0700

    Update JobDto.java

--- src/com/myclass/dto/JobDto.java
+++ src/com/myclass/dto/JobDto.java
@@ -3,7 +3,7 @@ package com.myclass.dto;
 import java.sql.Date;
 
 public class JobDto {
-	private String name;// test nà
+	private String name;// test , push git hub 
 	private String userDo;
 	private Date start_date;
 	private Date end_date;
